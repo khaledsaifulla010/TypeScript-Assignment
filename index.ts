3090153ca9f1073d5d2a7c9ec7f776766b7da98b
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-05-08 10:29:32 +0600

    solved problem 2

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
 function formatString(input: string, toUpper?: boolean): string {
-  
-  return toUpper===false? input.toLowerCase() : input.toUpperCase()
+  return toUpper === false ? input.toLowerCase() : input.toUpperCase();
 }
 
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
 
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+console.log(filterByRating(books));
